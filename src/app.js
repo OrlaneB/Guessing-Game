05faf035bfd19aba72873f1b6facf2376ddc0d7a
Laura Castro <Laura Castro>
2024-03-20 22:19:27 -0400

    changed button to circle shape

--- src/app.js
+++ src/app.js
@@ -48,23 +48,24 @@ let submitButton = document.getElementById('submit');
 let restartButton = document.createElement("button"); 
 
 // Setup guessing game landing page
+document.getElementById('circles').style.display = "flex";
+document.getElementById('circles').style.justifyContent = "center";
+document.getElementById('circles').style.alignItems = "center";
 
 // guessInput styles
 guessInput.style.backgroundColor = 'rgba(209,107,165, .3)';
 guessInput.style.borderRadius = '50%';
-guessInput.style.borderWidth = '10px';
 guessInput.style.borderStyle = 'solid';
 guessInput.style.borderColor = 'rgb(165,200,255)';
 guessInput.style.height = "500px";
 guessInput.style.width = "500px";
 guessInput.style.position = "relative";
-document.getElementById('input-div').style.display = "flex";
-document.getElementById('input-div').style.justifyContent = "center";
-document.getElementById('input-div').style.alignItems = "center";
-
 
 // submitButton styles
-submitButton.style.display = "block";
+submitButton.style.backgroundColor = 'rgba(209,107,165, .3)';
+submitButton.style.borderRadius = '50%';
+submitButton.style.height = "500px";
+submitButton.style.width = "500px";
 
 // resettButton styles
 restartButton.style.display = "block";
